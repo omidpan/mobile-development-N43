e93121b2d05ecd204ff9e103e3e70daf19a11fe9
leopn <omidpnteaching@gmail.com>
2026-02-17 10:03:56 -0500

    add button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,29 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 
@@ -37,7 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyAlignments(Modifier.padding(innerPadding))
+                    MyUIComponents(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -45,67 +31,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyAlignments(modifier: Modifier= Modifier){
-//    //01 Aligning Components Inside a Box
-//    Box(
-//        modifier = Modifier.fillMaxSize(),
-//        contentAlignment = Alignment.Center
-//    ) {
-//        Text(text = "Hello, Kotlin!")
-//    }
-//// 02 Adding Space Between Components
-//    Spacer(modifier = Modifier.height(20.dp))
-//    Column {
-//        Text("Hello ")
-//        Spacer(modifier = Modifier.height(20.dp)) // Adds space
-//        Text("Android")
-//    }
-////03 Spacer can also be used in Row for horizontal spacing:
-//
-//    Row {
-//        Text("Hello", modifier = Modifier.padding(start = 20.dp,end=30.dp))
-//        Spacer(modifier = Modifier.width(20.dp)) // Adds space
-//        Text("Android")
-//    }
+fun MyUIComponents(modifier: Modifier= Modifier) {
 
+    Button(
+        onClick = { /* Perform action here */ }
+    ) {
+        Text("Click Me")
+    }
 
 }
-
-
-//04 -summary
-//@Composable
-//fun MyAlignments(modifier: Modifier = Modifier){
-//    Column (modifier
-//        .fillMaxSize()
-//        .background(Color.White),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        Text(text = "Hello",
-//            color = Color.White,
-//            fontSize = 24.sp,
-//            fontWeight = FontWeight.Bold,
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier.background(Color.Red).padding(30.dp)
-//        )
-//        Spacer(modifier = Modifier.size(20.dp))
-//        Text(text = "Android",
-//            color = Color.White,
-//            fontSize = 24.sp,
-//            fontWeight = FontWeight.Bold,
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier.background(Color.Red).width(100.dp)
-//        )
-//        Spacer(modifier = Modifier.size(20.dp))
-//        Text(text = "Kotlin",
-//            color = Color.White,
-//            fontSize = 24.sp,
-//            fontWeight = FontWeight.Bold,
-//            textAlign = TextAlign.Center,
-//            modifier = Modifier.background(Color.Red).width(100.dp)
-//        )
-//    }
-//}
 
 
 
@@ -114,6 +48,6 @@ fun MyAlignments(modifier: Modifier= Modifier){
 @Composable
 fun CustomBoxWithModifierPreview() {
     HelloWorldTheme {
-        MyAlignments()
+        MyUIComponents()
     }
 }
