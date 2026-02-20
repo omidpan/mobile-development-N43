3bf97fda27f81d9ceb9a9ed0a0a69b28d82ac334
leopn <omidpnteaching@gmail.com>
2026-02-20 06:15:37 -0500

    image added

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 
@@ -23,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyUIComponents(Modifier.padding(innerPadding))
+                    MyUIComponentsImage(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,13 +36,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyUIComponents(modifier: Modifier= Modifier) {
+fun MyUIComponentsImage(modifier: Modifier= Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.first_image),
 
-    Button(
-        onClick = { /* Perform action here */ }
-    ) {
-        Text("Click Me")
-    }
+        contentDescription = "Scaled Image",
+        modifier = Modifier.size(300.dp),
+        contentScale = ContentScale.Fit
+
+    )
 
 }
 
@@ -48,6 +55,6 @@ fun MyUIComponents(modifier: Modifier= Modifier) {
 @Composable
 fun CustomBoxWithModifierPreview() {
     HelloWorldTheme {
-        MyUIComponents()
+        MyUIComponentsImage()
     }
 }
