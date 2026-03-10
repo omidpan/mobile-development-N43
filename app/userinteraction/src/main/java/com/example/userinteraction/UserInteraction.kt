7ed740d341a7e2aa7d7536bd4a964c2245aef35b
leopn <omidpnteaching@gmail.com>
2026-03-10 07:20:27 -0400

    add snackbar for user interaction with a button

--- app/userinteraction/src/main/java/com/example/userinteraction/UserInteraction.kt
+++ app/userinteraction/src/main/java/com/example/userinteraction/UserInteraction.kt
@@ -11,13 +11,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.userinteraction.ui.theme.HelloWorldTheme
+import kotlinx.coroutines.launch
 
 class UserInteraction : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +35,7 @@ class UserInteraction : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ToastMessageExample(modifier = Modifier.padding(innerPadding)
+                    SnackbarExample(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -34,29 +43,61 @@ class UserInteraction : ComponentActivity() {
     }
 }
 /*
-Activity Context: Tied to the screen's lifecycle.
-Used for UI-related tasks (dialogs, inflation).
+Scaffold
  */
 @Composable
-fun ToastMessageExample(modifier: Modifier = Modifier) {
+fun SnackbarExample(modifier: Modifier = Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
     val myContext = LocalContext.current
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            Toast.makeText(myContext, "This is a toast message", Toast.LENGTH_LONG).show()
-        }) {
-            Text("Show Toast Message")
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) { snackbarData ->
+                Snackbar(
+                    snackbarData = snackbarData,
+                    containerColor = Color.Gray,
+                    contentColor = Color.White,
+                    actionColor = Color.Black,
+                    dismissActionContentColor = Color.Black
+                )
+            }
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    val result = snackbarHostState.showSnackbar(
+                        message = "This is a Snackbar message",
+                        actionLabel = "Show Toast",
+                        duration = SnackbarDuration.Indefinite,
+                        withDismissAction = true
+                    )
+                    if (result == SnackbarResult.ActionPerformed) {
+                        Toast.makeText(
+                            myContext,
+                            "Action Performed!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }) {
+                Text("Show Snackbar Message")
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldTheme {
-        ToastMessageExample()
+        SnackbarExample()
     }
 }
